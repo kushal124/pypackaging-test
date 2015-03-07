75ab051223e2c17aef28d40cd594b6b393fc3b1d
Kushal Khandelwal <kushal124@gmail.com>
2015-03-07 22:23:06 +0100

    Packaging Learn

---
+++ funniest/__init__.py
@@ -0,0 +1,6 @@
+def testfunction():
+    '''
+    This is a test function for learning pythong packaging.
+
+    '''
+    return "TestFunction is being called"
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='funniest',
+        version='0.1',
+        description='A test package',
+        url = 'http:/github.com/kushal124/pypackagingtest',
+        author = 'Kushal',
+        author_email = "me@kushalkhandelwal.com",
+        license='MIT',
+        packages=['funniest'],
+        zip_safe=False)
+
